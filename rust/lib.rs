5fd232b4a0db9e9875410d55dc0f418e4fbe856c
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2021-08-22 22:20:47 +0200

    on char typed

--- rust/lib.rs
+++ rust/lib.rs
@@ -3,6 +3,10 @@ use serde::{Serialize, Deserialize};
 #[derive(Clone,Copy,Serialize,Deserialize,Debug)] pub struct TextRange { pub start: u32, pub end: u32 }
 pub use types::{SymbolKind, HlTag as HighlightTag, HlMod as HighlightModifier, Highlight};
 
+pub type FileId = u32;
+#[derive(Serialize,Deserialize)] pub struct FilePosition { file_id: FileId, offset: u32 }
+pub type TextEdit = Vec<(String, TextRange)>;
+
 #[derive(Serialize,Deserialize)] pub struct HighlightedRange {
   pub range: TextRange,
   pub highlight: Highlight,
@@ -22,36 +26,54 @@ pub use types::{SymbolKind, HlTag as HighlightTag, HlMod as HighlightModifier, H
 }
 
 pub trait Rust {
-	#[throws] fn highlight(&mut self, path: &Path) -> Vec<HighlightedRange>;
-	#[throws] fn definition(&self, path: &Path, offset: u32) -> Option<NavigationTarget>;
-	#[throws] fn diagnostics(&self, path: &Path) -> Vec<Diagnostic>;
+	#[throws] fn file_id(&self, path: &Path) -> Option<FileId>;
+	#[throws] fn highlight(&mut self, file_id: FileId) -> Vec<HighlightedRange>;
+	#[throws] fn diagnostics(&self, file_id: FileId) -> Vec<Diagnostic>;
+	#[throws] fn definition(&self, position: FilePosition) -> Option<NavigationTarget>;
+	#[throws] fn on_char_typed(&self, position: FilePosition, char_typed: char) -> Option<TextEdit>;
 }
 
-#[derive(Serialize,Deserialize)] pub struct HighlightFile { path: PathBuf }
+#[derive(Serialize,Deserialize)] pub struct GetFileId { path: PathBuf }
+impl ipc::Request for GetFileId {
+	type Server = Box<dyn Rust>;
+	type Reply = Option<FileId>;
+	#[throws] fn reply(self, server: &mut Self::Server) -> Self::Reply { server.file_id(&self.path)? }
+}
+
+#[derive(Serialize,Deserialize)] pub struct HighlightFile { file_id: FileId }
 impl ipc::Request for HighlightFile {
 	type Server = Box<dyn Rust>;
 	type Reply = Vec<HighlightedRange>;
-	#[throws] fn reply(self, server: &mut Self::Server) -> Self::Reply { server.highlight(&self.path)? }
+	#[throws] fn reply(self, server: &mut Self::Server) -> Self::Reply { server.highlight(self.file_id)? }
 }
 
-#[derive(Serialize,Deserialize)] pub struct Definition { path: PathBuf, offset: u32 }
-impl ipc::Request for Definition {
-	type Server = Box<dyn Rust>;
-	type Reply = Option<NavigationTarget>;
-	#[throws] fn reply(self, server: &mut Self::Server) -> Self::Reply { server.definition(&self.path, self.offset)? }
-}
-
-#[derive(Serialize,Deserialize)] pub struct Diagnostics { path: PathBuf }
+#[derive(Serialize,Deserialize)] pub struct Diagnostics { file_id: FileId }
 impl ipc::Request for Diagnostics {
 	type Server = Box<dyn Rust>;
 	type Reply = Vec<Diagnostic>;
-	#[throws] fn reply(self, server: &mut Self::Server) -> Self::Reply { server.diagnostics(&self.path)? }
+	#[throws] fn reply(self, server: &mut Self::Server) -> Self::Reply { server.diagnostics(self.file_id)? }
+}
+
+#[derive(Serialize,Deserialize)] pub struct Definition { position: FilePosition }
+impl ipc::Request for Definition {
+	type Server = Box<dyn Rust>;
+	type Reply = Option<NavigationTarget>;
+	#[throws] fn reply(self, server: &mut Self::Server) -> Self::Reply { server.definition(self.position)? }
+}
+
+#[derive(Serialize,Deserialize)] pub struct OnCharTyped { position: FilePosition, char_typed: char }
+impl ipc::Request for OnCharTyped {
+	type Server = Box<dyn Rust>;
+	type Reply = Option<TextEdit>;
+	#[throws] fn reply(self, server: &mut Self::Server) -> Self::Reply { server.on_char_typed(self.position, self.char_typed)? }
 }
 
 #[derive(Serialize,Deserialize)] pub enum Item {
+	GetFileId(GetFileId),
 	HighlightFile(HighlightFile),
-	Definition(Definition),
 	Diagnostics(Diagnostics),
+	Definition(Definition),
+	OnCharTyped(OnCharTyped)
 }
 
 use {fehler::throws, error::Error};
@@ -63,23 +85,32 @@ impl ipc::Server for Box<dyn Rust> {
 		#[throws] fn serialize<T:Serialize>(r : Result<T, Error>) -> Vec<u8> { ipc::serialize(&r.map_err(|e| e.to_string()))? }
 		use {ipc::Request, Item::*};
 		match item {
+			GetFileId(r) => serialize(r.reply(self)),
 			HighlightFile(r) => serialize(r.reply(self)),
-			Definition(r) => serialize(r.reply(self)),
 			Diagnostics(r) => serialize(r.reply(self)),
+			Definition(r) => serialize(r.reply(self)),
+			OnCharTyped(r) => serialize(r.reply(self)),
 		}?
 	}
 }
 
-fn abs(path: &Path) -> PathBuf { if path.is_relative() { std::env::current_dir().unwrap().join(path) } else { path.to_owned() } }
-
-#[throws] pub fn highlight(path: &Path) -> Vec<HighlightedRange> {
-	ipc::request::<HighlightFile>(Item::HighlightFile(HighlightFile{path: abs(path)}))?
+#[throws] pub fn file_id(path: &Path) -> Option<FileId> {
+	fn abs(path: &Path) -> PathBuf { if path.is_relative() { std::env::current_dir().unwrap().join(path) } else { path.to_owned() } }
+	ipc::request::<GetFileId>(Item::GetFileId(GetFileId{path: abs(path)}))?
 }
 
-#[throws] pub fn definition(path: &Path, offset: usize) -> Option<NavigationTarget> {
-	ipc::request::<Definition>(Item::Definition(Definition{path: abs(path), offset: offset as u32}))?
+#[throws] pub fn highlight(file_id: FileId) -> Vec<HighlightedRange> {
+	ipc::request::<HighlightFile>(Item::HighlightFile(HighlightFile{file_id}))?
 }
 
-#[throws] pub fn diagnostics(path: &Path) -> Vec<Diagnostic> {
-	ipc::request::<Diagnostics>(Item::Diagnostics(Diagnostics{path: abs(path)}))?
+#[throws] pub fn diagnostics(file_id: FileId) -> Vec<Diagnostic> {
+	ipc::request::<Diagnostics>(Item::Diagnostics(Diagnostics{file_id}))?
+}
+
+#[throws] pub fn definition(position: FilePosition) -> Option<NavigationTarget> {
+	ipc::request::<Definition>(Item::Definition(Definition{position}))?
+}
+
+#[throws] pub fn on_char_typed(position: FilePosition, char_typed: char) -> Option<TextEdit> {
+	ipc::request::<OnCharTyped>(Item::OnCharTyped(OnCharTyped{position, char_typed}))?
 }
--- server/main.rs
+++ server/main.rs
@@ -1,7 +1,12 @@
 #![feature(default_free_fn)]
 use {std::default::default, fehler::throws, anyhow::Error, std::path::{Path, PathBuf}};
 
-fn from(range: &text_size::TextRange) -> rust::TextRange { rust::TextRange{start: range.start().into(), end: range.end().into()} } // serde
+// Direct Serde on the foreign types would avoid boilerplate
+trait From<T> { fn from(T) -> Self; }
+trait Into<T:From<Self>> { fn into(self) -> T { T::from(self) } }
+impl From<&text_size::TextRange> for rust::TextRange { fn from(range: &text_size::TextRange) -> Self { Self{start: range.start().into(), end: range.end().into()} } }
+impl From<TextEdit> for rust::TextEdit { fn from(text_edit: &text_edit::TextEdit) -> Self { text_edit.iter().map(|text_edit::Indel{insert,delete} (insert, delete.into())).collect() } }
+impl From<rust::FilePosition> for ide::FilePosition { fn from(rust::FilePosition{file_id, offset}: rust::FilePosition) -> Self { Self{file_id: vfs::FileId(file_id), offset: offset.into()} } }
 
 struct Analyzer {
 		host: ide::AnalysisHost,
@@ -12,22 +17,22 @@ impl Analyzer {
 #[throws] fn new() -> Self {
 	use rust_analyzer::cli::load_cargo::*;
 	let (host, vfs, _proc_macro) = load_workspace_at(&std::env::current_dir()?, &default(),
-		&LoadCargoConfig{load_out_dirs_from_check: false, with_proc_macro: false, prefill_caches: false}, &|_| {})?;
+		&LoadCargoConfig{load_out_dirs_from_check: false, with_proc_macro: true, prefill_caches: false}, &|_| {})?;
 	Analyzer{host, vfs}
 }
 #[throws] fn file_id(&self, path: &Path) -> vfs::FileId {
 	let current_path = std::env::current_dir().unwrap().join(path);
 	let path = if path.is_relative() { current_path.as_path() } else { path };
 	use std::convert::TryFrom;
-	let (file_id, _) = self.vfs.iter().find(|&(_, p)| p.as_path().unwrap() == <&paths::AbsPath>::try_from(path).unwrap()).ok_or(Error::msg(path.to_str().unwrap().to_owned()))?;
-	file_id
+	self.vfs.file_id(paths::AbsPath::try_from(path).unwrap().into())
 }
 #[throws] fn path(&self, id: &vfs::FileId) -> PathBuf { Path::new(self.vfs.file_path(*id).as_path().unwrap().as_ref().to_str().unwrap()).to_owned() }
 }
 
 impl rust::Rust for Analyzer {
-	#[throws] fn highlight(&mut self, path: &Path) -> Vec<rust::HighlightedRange> {
-		let file_id = self.file_id(path)?;
+	#[throws] fn file_id(&mut self, path: Path) -> rust::FileId { self.file_id(path)?.0 }
+	#[throws] fn highlight(&mut self, file_id: rust::FileId) -> Vec<rust::HighlightedRange> {
+		let file_id = vfs::FileId(file_id);
 		let mut change = ide::Change::new();
 		change.change_file(file_id, Some(std::sync::Arc::new(std::str::from_utf8(&std::fs::read(path)?)?.to_owned())));
 		self.host.apply_change(change);
@@ -35,13 +40,17 @@ impl rust::Rust for Analyzer {
 		//trace::timeout_(100, || self.host.analysis().highlight(file_id), format!("Timeout: {}", path.display().to_string()))??
 			.into_iter().map(|ide::HlRange{range, highlight, ..}| rust::HighlightedRange{range: from(&range), highlight}).collect::<Vec<_>>()
 	}
-	#[throws] fn definition(&self, path: &Path, offset: u32) -> Option<rust::NavigationTarget> {
-		self.host.analysis().goto_definition(ide::FilePosition{file_id: self.file_id(path)?, offset: offset.into()})?
+	#[throws] fn definition(&self, position: rust::FilePosition) -> Option<rust::NavigationTarget> {
+		self.host.analysis().goto_definition(position.into())?
 		.map(|v| v.info.first().map(|ide::NavigationTarget{file_id, full_range, ..}| rust::NavigationTarget{path: self.path(file_id).unwrap(), range: from(full_range)})).flatten()
 	}
-	#[throws] fn diagnostics(&self, path: &Path) -> Vec<rust::Diagnostic> {
-		self.host.analysis().diagnostics(&default(), ide::AssistResolveStrategy::None, self.file_id(path)?)?
+	#[throws] fn diagnostics(&self, file_id: rust::FileId) -> Vec<rust::Diagnostic> {
+		self.host.analysis().diagnostics(&default(), ide::AssistResolveStrategy::None, vfs::FileId(file_id))?
 			.into_iter().map(|ide::Diagnostic{message, range, ..}| rust::Diagnostic{message, range: from(&range)}).collect::<Vec<_>>()
+	}
+	#[throws] fn on_typed_char(&self, position: rust::FilePosition, typed_char: char) -> Option<TextEdit> {
+		if char=='\n' { self.host.analysis().on_enter(position.into())?.into() }
+		else { panic!() }
 	}
 }
 
